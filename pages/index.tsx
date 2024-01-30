860ec7eef7a84b562526563b667e5bfdcc613aa2
Ian <ianmillshumphrey@gmail.com>
2024-01-30 14:24:51 -0600

    scroll bug fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,15 +33,17 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
-  const { logout, loading } = useAuth()
-  const showModal = useRecoilValue(modalState)
+  const { logout, loading } = useAuth();
+  const showModal = useRecoilValue(modalState);
 
-  if (loading) return (
-    null
-  )
+  if (loading) return null;
 
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${
+        showModal && "h-screen overflow-hidden"
+      }`}
+    >
       <Header />
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 md:pt-24">
         <Banner netflixOriginals={netflixOriginals} />
